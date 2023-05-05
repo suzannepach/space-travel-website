8707df1b016b5c16e759cfe950f6781e1b34a04e
Suzanne Pach <51491480+suzannepach@users.noreply.github.com>
2023-05-05 21:45:20 +0200

    Added javascript to make mobile menu appear and dissapear

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // javascript
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+    }
+})
