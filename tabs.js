ef377fd066c2960ca4b22ba269f3d31a2d2a9a72
Suzanne Pach <51491480+suzannepach@users.noreply.github.com>
2023-05-11 12:55:45 +0200

    Added keyboard navigation for the tabs

---
+++ tabs.js
@@ -0,0 +1,31 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', changeTabFocus)
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+    const keyDownLeft = 37;
+    const keyDownRight = 39;
+
+    if (e.keyCode === keyDownLeft || e.keyCode === keyDownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
+    
+    if (e.keyCode === keyDownRight) {
+        tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
+    }
+
+    if (e.keyCode === keyDownLeft) {
+        tabFocus--;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }        
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+}
